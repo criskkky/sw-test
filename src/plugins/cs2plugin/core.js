6ce20fa9120ba5e480cbbf8c3b3afff21b082891
criskkky <criskywww@gmail.com>
2025-03-03 18:29:05 -0300

    Create core.js

---
+++ src/plugins/cs2plugin/core.js
@@ -0,0 +1,15 @@
+function GetPluginAuthor() {
+    return "SwiftlyCS2 Development"
+}
+
+function GetPluginVersion() {
+    return "1.0.3"
+}
+
+function GetPluginName() {
+    return "Brand New Lua Plugin"
+}
+
+function GetPluginWebsite() {
+    return "https://swiftlycs2.net"
+}
