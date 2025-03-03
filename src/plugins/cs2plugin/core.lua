61c6f3317220e02a6f3255624fbb61f746593ef6
criskkky <criskywww@gmail.com>
2025-03-03 16:47:02 -0300

    Create core.lua

---
+++ src/plugins/cs2plugin/core.lua
@@ -0,0 +1,15 @@
+function GetPluginAuthor()
+	return "criskkky"
+end
+
+function GetPluginVersion()
+	return "v1.0.0"
+end
+
+function GetPluginName()
+	return "Test"
+end
+
+function GetPluginWebsite()
+	return "https://github.com/criskkky/sw-test"
+end
